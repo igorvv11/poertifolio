5ea4bc4a7783d1e6bd2d3685b150938e975f6e9a
Igor Viana <igor.viana560@gmail.com>
2025-08-14 17:48:41 -0300

    estilizando a sidebar

--- portifolio/src/containers/Sidebar/index.tsx
+++ portifolio/src/containers/Sidebar/index.tsx
@@ -1,9 +1,22 @@
+import Avatar from '../../components/Avatar'
+import Paragrafo from '../../components/Paragrafo'
 import Titulo from '../../components/Titulo'
+import { BotaoTema, Descricao, SideBarContainer } from './styles'
 
 const Sidebar = () => (
   <aside>
-    <img src="https://github.com/igorvv11.png" />
-    <Titulo fontSize={20}>Igor Viana</Titulo>
+    <SideBarContainer>
+      <Avatar />
+      <Titulo fontSize={20}>Igor Viana</Titulo>
+      <Paragrafo tipo="secundario" fontSize={16}>
+        Igorvv11
+      </Paragrafo>
+      <Descricao tipo="principal">Desenvolverdor Full-Stack Java</Descricao>
+      <Paragrafo tipo="principal" fontSize={10}>
+        Email:Igor.viana560@gmail.com Telefone: 81 99281-4280
+      </Paragrafo>
+      <BotaoTema>Trocar tema</BotaoTema>
+    </SideBarContainer>
   </aside>
 )
 
